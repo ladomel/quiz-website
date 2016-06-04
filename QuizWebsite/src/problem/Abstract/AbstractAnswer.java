82c5e60f2f3274565e8c3bfe27018b9eb49825c4
Andrew Niedens <niedens.a@gmail.com>
2016-06-04 15:09:52 +0400

    AbstractAnswer implemented

---
+++ QuizWebsite/src/problem/Abstract/AbstractAnswer.java
@@ -0,0 +1,67 @@
+package problem.Abstract;
+
+public abstract class AbstractAnswer {
+	private int grade;
+	private boolean graded = false;
+	private int testId;
+	private Object userAnswer;
+
+
+	/**
+	 *  Constructor stores test's id number and user's answer object.
+	 */
+	public AbstractAnswer(int newTestId, Object newUserAnswer) 
+	{
+		testId = newTestId;
+		userAnswer = newUserAnswer;
+	}
+
+	/**
+	 * Returns an object with all information about Answer.
+	 * @return answer.
+	 */
+	public Object getAnswer()
+	{	
+		return userAnswer;
+	}
+
+	/**
+	 * Sets the grade user in object.
+	 */
+	public void setGrade(int newGrade)
+	{
+		grade = newGrade;
+		graded = true;
+	}
+
+	/**
+	 * Returns id of the test.
+	 * @return id of test.
+	 */
+	public int getTestId()
+	{
+		return testId;
+	}
+
+	/**
+	 * Returns grade of user or null if answer was not graded yet.
+	 * @return grade or null.
+	 */
+	public Integer getGrade()
+	{
+		if(graded) return grade;
+		return null;
+	}
+
+	/**
+	 * Returns true if answer was graded.
+	 * @return graded.
+	 */
+	public boolean isGraded()
+	{
+		return graded;
+	}
+
+
+
+}
