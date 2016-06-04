1bfa89f7805a83411024b2acd25f687cc11a3109
Andrew Niedens <niedens.a@gmail.com>
2016-06-04 15:49:35 +0400

    Added abstract Statement

---
+++ QuizWebsite/src/problem/Abstract/Statement.java
@@ -0,0 +1,10 @@
+package problem.Abstract;
+
+public abstract class Statement {
+	
+	/**
+	 * Returns string description of the problem.
+	 * @return Problem description string.
+	 */
+	public abstract String getDescription();
+}
