d7d393e275745c1bd82313489d9452c27771ca69
Andrew Niedens <niedens.a@gmail.com>
2016-06-04 22:31:09 +0400

    Added User class with private variables only

---
+++ QuizWebsite/src/classes/User.java
@@ -0,0 +1,15 @@
+package classes;
+
+import java.util.Set;
+
+public class User {
+	private String status; // TODO enum; status of the user. "Ordinary", "Administration" etc.
+	private String userName;
+	private String hashedPassword;
+	private Set<Integer> friends; // Set of user's friends.
+	private Set<Integer> createdQuizes;	
+	private Set<Integer> takenQuizes;
+	private Set<Integer> achievements;
+	private Set<Integer> messages; // Messages stored here. might change, depends on implementation
+	
+}
