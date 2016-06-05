ffba67cb004e3caf498d1174fe3ef1cf1c4d9048
Andrew Niedens <niedens.a@gmail.com>
2016-06-05 13:09:57 +0400

    Added User constructor

--- QuizWebsite/src/classes/User.java
+++ QuizWebsite/src/classes/User.java
@@ -3,6 +3,29 @@ package classes;
 import java.util.Set;
 
 public class User {
+	public User(String userName, String hashedPassword)
+	{
+		setUserName(userName);
+		setHashedPassword(hashedPassword); // Set registration date.
+	}
+	
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getHashedPassword() {
+		return hashedPassword;
+	}
+
+	public void setHashedPassword(String hashedPassword) {
+		this.hashedPassword = hashedPassword;
+	}
+
+
 	private String status; // TODO enum; status of the user. "Ordinary", "Administration" etc.
 	private String userName;
 	private String hashedPassword;
