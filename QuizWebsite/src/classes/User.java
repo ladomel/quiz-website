44c45cb2d84d0e15928e46edb7381208af3294e2
Andrew Niedens <niedens.a@gmail.com>
2016-06-06 09:31:24 +0400

    Added getters and setters for user.

--- QuizWebsite/src/classes/User.java
+++ QuizWebsite/src/classes/User.java
@@ -4,12 +4,28 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class User {
+
+	public enum Status{REGULAR, ADMINISTRATION} 
+	
+	private Status status;
+	private String userName;
+	private String hashedPassword;
+	private HashSet<Integer> friends; // Set of user's friends.
+	private HashSet<Integer> createdQuizes;	
+	private HashSet<Integer> takenQuizes;
+	private HashSet<Integer> achievements;
+	private HashSet<Integer> messages; // Messages stored here. might change, depends on implementation	
+	
 	public User(String userName, String hashedPassword)
 	{
 		setUserName(userName);
-		setHashedPassword(hashedPassword); // Set registration date.
-		status = "regular"; // TODO change
-		friends = new HashSet<Integer>();
+		setHashedPassword(hashedPassword); 
+		setStatus(Status.REGULAR);  // By default, user is regular.
+		setFriends(new HashSet<Integer>());
+		createdQuizes = new HashSet<Integer>();
+		takenQuizes = new HashSet<Integer>();
+		achievements = new HashSet<Integer>();
+		messages = new HashSet<Integer>();
 	}
 	
 	public String getUserName() {
@@ -27,13 +43,27 @@ public class User {
 		this.hashedPassword = hashedPassword;
 	}
 
+	public Status getStatus() {
+		return status;
+	}
 
-	private String status; // TODO enum; status of the user. "Ordinary", "Administration" etc.
-	private String userName;
-	private String hashedPassword;
-	private HashSet<Integer> friends; // Set of user's friends.
-	private HashSet<Integer> createdQuizes;	
-	private HashSet<Integer> takenQuizes;
-	private HashSet<Integer> achievements;
-	private HashSet<Integer> messages; // Messages stored here. might change, depends on implementation	
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public HashSet<Integer> getFriends() {
+		return friends;
+	}
+
+	public void addFriend(Integer friendId) {
+		this.getFriends().add(friendId);
+	}
+	
+	public void removeFriend(Integer friendId) {
+		this.getFriends().remove(friendId);
+	}
+
+	private void setFriends(HashSet<Integer> friends) {
+		this.friends = friends;
+	}
 }
