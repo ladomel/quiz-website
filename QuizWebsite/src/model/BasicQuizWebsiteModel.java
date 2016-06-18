505fb7a9d14a45e709a8046df3f3eadf733dc2fb
Sergi Chalauri <schal13@freeuni.edu.ge>
2016-06-18 20:17:14 +0400

    Basic model implemented.
    Uses map and list.

--- QuizWebsite/src/model/BasicQuizWebsiteModel.java
+++ QuizWebsite/src/model/BasicQuizWebsiteModel.java
@@ -1,5 +1,10 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import classes.Quiz;
 import classes.User;
 
@@ -7,10 +12,15 @@ public class BasicQuizWebsiteModel implements QuizWebsiteModelInterface {
 
 	private static BasicQuizWebsiteModel singletonInstance;
 	
+	private Map<String, User> users;
+	private List<Quiz> quizes;
+	
 	/*
 	 * Because fuck public. 
 	 */
 	private BasicQuizWebsiteModel() {
+		users = new HashMap<String, User> ();
+		quizes = new ArrayList<Quiz> ();
 	}
 	
 	/**
@@ -27,44 +37,39 @@ public class BasicQuizWebsiteModel implements QuizWebsiteModelInterface {
 	
 	@Override
 	public User getUser(String userName) {
-		// TODO Auto-generated method stub
-		return null;
+		return users.get(userName);
 	}
 
 	@Override
 	public void deleteUser(String userName) {
-		// TODO Auto-generated method stub
-
+		users.remove(userName);
 	}
 
 	@Override
 	public void addUser(User user) {
-		// TODO Auto-generated method stub
-
+		users.put(user.getUserName(), user);
 	}
 
 	@Override
 	public Quiz getQuiz(int quizId) {
-		// TODO Auto-generated method stub
-		return null;
+		return quizes.get(quizId);
 	}
 
 	@Override
 	public void replaceQuiz(Quiz quiz, int quizId) {
-		// TODO Auto-generated method stub
-
+		quizes.remove(quizId);
+		quizes.add(quizId, quiz);
 	}
 
 	@Override
 	public int addQuiz(Quiz quiz) {
-		// TODO Auto-generated method stub
-		return 0;
+		quizes.add(quiz);
+		return quizes.size() - 1;
 	}
 
 	@Override
 	public void deleteQuiz(int quizId) {
-		// TODO Auto-generated method stub
-
+		replaceQuiz(null, quizId);
 	}
 
 }
