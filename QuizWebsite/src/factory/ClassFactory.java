c5142f906c6697e4e950d43081b38e8012e0a730
Andrew Niedens <niedens.a@gmail.com>
2016-06-25 00:19:42 +0400

    Added getQuestionQR function

--- QuizWebsite/src/factory/ClassFactory.java
+++ QuizWebsite/src/factory/ClassFactory.java
@@ -1,8 +1,10 @@
 package factory;
 import java.util.ArrayList;
+import java.util.Set;
 
 import classes.Quiz;
 import classes.User;
+import classes.question.QuestionQR;
 
 public class ClassFactory {
 	public User getUser(String userName, String password, String salt)
@@ -10,10 +12,13 @@ public class ClassFactory {
 		return new User(userName, password, salt);
 	}
 	
-	public Quiz getQuiz(String userName, String quizName, String description, ArrayList<Integer> problems)
+	public Quiz getQuiz(String userName, String quizName, String description)
 	{
-		return new Quiz(userName, quizName, description, problems);
+		return new Quiz(userName, quizName, description);
 	}
 	
-	
+	public QuestionQR getQuestionQR(String newProblem, int grade, Set<String> newAnswers)
+	{
+		return new QuestionQR(newProblem, grade, newAnswers);
+	}
 }
