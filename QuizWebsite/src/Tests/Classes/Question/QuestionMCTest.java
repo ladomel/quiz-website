7ce0c968be45c2cfbcb52db3f1e895ca2b12ae52
Andrew Niedens <niedens.a@gmail.com>
2016-06-25 17:52:16 +0400

    QuestionMCTest implemented

---
+++ QuizWebsite/src/Tests/Classes/Question/QuestionMCTest.java
@@ -0,0 +1,110 @@
+package Tests.Classes.Question;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import classes.question.QuestionMC;
+
+public class QuestionMCTest {
+
+	private Set<String> wrongAnswers;
+	private String description;
+	private String correctAnswer;
+	private int grade;
+	private List<String> userAnswer;
+	private QuestionMC question;
+	
+	@Before
+	public void init()
+	{
+		userAnswer = new ArrayList<String>(); 	
+		wrongAnswers = new HashSet<String>();
+		
+		wrongAnswers.add("b");
+		wrongAnswers.add("c");
+		wrongAnswers.add("d");
+		
+		description = "Problem";
+		correctAnswer = "a";
+		grade = 5;
+		
+		question = new QuestionMC(description, grade, correctAnswer, wrongAnswers);
+	}
+	
+	@Test
+	public void testCorrectAnswers() {
+		assertEquals((int)question.getGrade(question.getCorrectAnswers()), (int)question.getMaxGrade());
+	}
+
+	@Test
+	public void testBasic() {
+		assertEquals(1, question.getCorrectAnswers().size());
+		assertTrue(correctAnswer.equals(question.getCorrectAnswers().get(0))); 
+		assertEquals(grade, question.getGrade());	
+		assertEquals(grade, question.getMaxGrade());
+		assertEquals(description, question.getProblem());
+		assertTrue(question.getWrongAnswers().equals(wrongAnswers));
+		assertTrue(question.getCorrectAnswers().get(0).equals(correctAnswer));
+		assertTrue(question.getCorrectAnswer().equals(correctAnswer));
+	}
+	
+	@Test
+	public void testGetGrade1() { 
+		userAnswer.add("a");	
+		assertEquals(grade, (int)question.getGrade(userAnswer));
+	}
+	
+	@Test
+	public void testGetGrade2() { 
+		userAnswer.add("b");	
+		assertEquals(0, (int)question.getGrade(userAnswer));
+	}
+	
+	@Test
+	public void testGetGrade3() { 
+		userAnswer.add("l");	
+		assertEquals(0, (int)question.getGrade(userAnswer));
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void illegalArgumentTest1() { 	
+		correctAnswer = null;
+		new QuestionMC(description, grade, correctAnswer, wrongAnswers);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void illegalArgumentTest2() { 	
+		wrongAnswers = null;
+		new QuestionMC(description, grade, correctAnswer, wrongAnswers);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void illegalArgumentTest3() { 	
+		wrongAnswers.add(null);
+		new QuestionMC(description, grade, correctAnswer, wrongAnswers);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void illegalArgumentTest4() { 	
+		question.getGrade(userAnswer);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void illegalArgumentTest5() { 
+		userAnswer.add(null);
+		question.getGrade(userAnswer);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void illegalArgumentTest6() { 
+		userAnswer = null;
+		question.getGrade(userAnswer);
+	}
+}
