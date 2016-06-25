b33120e1dc8d8106467a613f11b4e1b6978c1810
Andrew Niedens <niedens.a@gmail.com>
2016-06-25 10:43:08 +0400

    GetMaxGrade function added

--- QuizWebsite/src/classes/question/Abstract/Question.java
+++ QuizWebsite/src/classes/question/Abstract/Question.java
@@ -5,25 +5,33 @@ import java.util.List;
 public abstract class Question{
 
 	private String problem;
-	private int grade; // Grade for each correct answer.
+	private int grade; 
 	
 	/**
-	 * @param newProblem problem description of the class.
+	 * @param newProblem - problem description of the class
+	 * @param grade - grade for each correct answer
 	 */
-	protected Question(String newProblem, int grade)
+	public Question(String newProblem, int grade)
 	{
 		setProblem(newProblem);
 		setGrade(grade);
 	}
 	
 	/**
-	 * Returns problem for user to see.	
-	 * @return problem.
+	 * Returns problem.	
+	 * 
+	 * @return problem - problem for user to see
 	*/
 	public String getProblem()
 	{
 		return problem;
 	}
+	
+	/**
+	 * Sets problem.	
+	 * 
+	 * @param newProblem - problem for user to see
+	*/
 	
 	private void setProblem(String newProblem)
 	{
@@ -31,23 +39,42 @@ public abstract class Question{
 	}
 	
 	/**
-	 * Returns List's grade, or null if wrong format.	
-	 * @param answer List of user's filled fields.
-	 * @return grade of answer.
+	 * Returns List's grade, or null if wrong format.
+	 * 	
+	 * @param answer - List of user's filled fields
+	 * @return grade - grade of answer
 	*/
-	public abstract Integer getGrade(List <String> answer);
+	public abstract Integer getGrade(List<String> answer);
 	
 	/**
 	 * This function returns List of correct answers to show user.
-	 * @return List of correct answers.
+	 * 
+	 * @return answers - List of correct answers
 	*/
 	public abstract List<String> getCorrectAnswers();
-
+	
+	/**
+	 * This function returns grade for each correct answer.
+	 * 
+	 * @return grade - grade for each correct answer
+	*/
 	public int getGrade() {
 		return grade;
 	}
 
+	/**
+	 * This function sets grade for each correct answer.
+	 * 
+	 * @param grade - grade for each correct answer
+	*/
 	public void setGrade(int grade) {
 		this.grade = grade;
 	}
+	
+	/**
+	 * This function returns maximum grade user can get in the question.
+	 * 
+	 * @return grade - maximum grade user can get in the question
+	 */
+	public abstract int getMaxGrade();
 }
