0f0b2d2316e64efa492ef44f8d3cbb658cb278fa
Lado <lmeli14@freeuni.edu.ge>
2016-06-26 16:46:20 +0400

    Added some constants

--- QuizWebsite/src/Main/Constants.java
+++ QuizWebsite/src/Main/Constants.java
@@ -4,6 +4,7 @@ public interface Constants {
 
 	// Website name
 	public static final String SITE_NAME = "QuizWebsite";
-
-	
+	public static final String BLANK = " _____ ";
+	public static final int MAX_DISPLAY = 20;
+	public static final int TIME_PERIOD = 7;	
 }
