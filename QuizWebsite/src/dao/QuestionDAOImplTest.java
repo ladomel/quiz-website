d2b83f34ff3ad5ee564655611b18ed34509ec145
Sergi Chalauri <schal13@freeuni.edu.ge>
2016-06-28 11:11:56 +0400

    File to run question DAO for checking

---
+++ QuizWebsite/src/dao/QuestionDAOImplTest.java
@@ -0,0 +1,36 @@
+package dao;
+
+import static org.junit.Assert.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import factory.ClassFactory;
+
+public class QuestionDAOImplTest {
+
+	private DAOInstances factory;
+	private QuestionDAO questionDAO;
+	private ClassFactory classFactory;
+
+	@Before
+	public void setUp() throws Exception {
+		factory = new DAOInstances();
+		factory.init();
+		questionDAO = factory.getQuestionDAO();
+		classFactory = new ClassFactory();
+	}
+
+	@Test
+	public void test() {
+//		Set<String> set = new HashSet<String>();
+//		set.add("bonapart");
+//		set.add("neipl");
+//		questionDAO.addQR(4, classFactory.getQuestionQR("didie", 2, set));
+		questionDAO.getQuestions(4);
+	}
+
+}
