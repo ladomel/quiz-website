bdad1f80d236a14b9ba07a146170b414654a3b62
Sergi Chalauri <schal13@freeuni.edu.ge>
2016-06-28 23:21:51 +0400

    Implemented result insertion

--- QuizWebsite/src/dao/ResultDAOImpl.java
+++ QuizWebsite/src/dao/ResultDAOImpl.java
@@ -7,8 +7,7 @@ import java.util.List;
 
 import javax.sql.DataSource;
 
-import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
-
+import classes.Answer;
 import classes.Result;
 
 public class ResultDAOImpl implements ResultDAO {
@@ -23,20 +22,72 @@ public class ResultDAOImpl implements ResultDAO {
 	public void insertResult(Result result) {
 		try {
 			Connection con = dataSource.getConnection();
-			PreparedStatement preparedStatement = 
-					con.prepareStatement(inserResultCommand());
-			preparedStatement.setString(1, result.getUserName());
-			preparedStatement.setInt(2, result.getQuizId());
-			preparedStatement.setInt(3, result.getFinalGrade());
-			preparedStatement.setLong(4, result.getTimeStarted());
-			preparedStatement.setLong(5, result.getTimeTaken());
-			preparedStatement.executeUpdate();
+			insertResultWithoutAnswers(result, con);
+			int resultId = MySQLUtil.getLastInsertId(con);
+			insertAnswers(resultId, result.getAnswers(), con);
 			con.close();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
+	}
+
+	/*
+	 * Here are used many inserts instead of single insert with several values.
+	 * Performance difference should be negligible because of low
+	 * numbers involved with quizzes in general.
+	 */
+	private void insertAnswers(int resultId, List<Answer> answers,
+			Connection con) throws SQLException {
+		// for each question
+		for(int questionId = 0; questionId < answers.size(); questionId++) {
+			int grade = answers.get(questionId).getGrade();
+			insertQuestionGrade(con, resultId, questionId, grade);
+			List<String> answerTexts = answers.get(questionId).getAnswer();
+			insertQuestionFieldTexts(con, questionId, answerTexts);
+		}
+		
+	}
+	
+	private void insertQuestionFieldTexts(Connection con, int questionId, 
+			List<String> answerTexts) throws SQLException {
+		// for each field
+		for(int fieldId = 0; fieldId < answerTexts.size(); fieldId++) {
+			PreparedStatement preparedStatement =
+					con.prepareStatement("INSERT INTO user_answers "
+							+ "(question_id, field_id, user_answer) "
+							+ "VALUES(?, ?, ?);");
+			preparedStatement.setInt(1, questionId);
+			preparedStatement.setInt(2, fieldId);
+			String text = answerTexts.get(fieldId);
+			preparedStatement.setString(3, text );
+			preparedStatement.executeUpdate();
+		}
+	}
+
+	private void insertQuestionGrade(Connection con, int resultId, int i,
+			Integer grade) throws SQLException {
+		PreparedStatement preparedStatement = 
+				con.prepareStatement("INSERT INTO answers_of_question " + 
+						"(result_id, question_id, grade) " + 
+						"VALUES(?, ?, ?);");
+		preparedStatement.setInt(1, resultId);
+		preparedStatement.setInt(2, i);
+		preparedStatement.setInt(3, grade);
+		preparedStatement.executeUpdate();
+	}
+
+	private void insertResultWithoutAnswers(Result result, Connection con)
+			throws SQLException {
+		PreparedStatement preparedStatement = 
+				con.prepareStatement(inserResultCommand());
+		preparedStatement.setString(1, result.getUserName());
+		preparedStatement.setInt(2, result.getQuizId());
+		preparedStatement.setInt(3, result.getFinalGrade());
+		preparedStatement.setLong(4, result.getTimeStarted());
+		preparedStatement.setLong(5, result.getTimeTaken());
+		preparedStatement.executeUpdate();
 	}
 
 	private String inserResultCommand() {
