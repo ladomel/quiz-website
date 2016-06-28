e04f87488792e2493e0253448d3abdcda0b77daa
Sergi Chalauri <schal13@freeuni.edu.ge>
2016-06-28 19:16:51 +0400

    Fixed basic data source -> data source

--- QuizWebsite/src/dao/ResultDAOImpl.java
+++ QuizWebsite/src/dao/ResultDAOImpl.java
@@ -1,5 +1,8 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 import javax.sql.DataSource;
@@ -12,14 +15,35 @@ public class ResultDAOImpl implements ResultDAO {
 
 	private DataSource dataSource;
 	
-	public ResultDAOImpl(BasicDataSource dataSource) {
+	public ResultDAOImpl(DataSource dataSource) {
 		this.dataSource = dataSource;
 	}
 
 	@Override
 	public void insertResult(Result result) {
-		// TODO Auto-generated method stub
+		try {
+			Connection con = dataSource.getConnection();
+			PreparedStatement preparedStatement = 
+					con.prepareStatement(inserResultCommand());
+			preparedStatement.setString(1, result.getUserName());
+			preparedStatement.setInt(2, result.getQuizId());
+			preparedStatement.setInt(3, result.getFinalGrade());
+			preparedStatement.setLong(4, result.getTimeStarted());
+			preparedStatement.setLong(5, result.getTimeTaken());
+			preparedStatement.executeUpdate();
+			con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 
+	private String inserResultCommand() {
+		return "INSERT INTO results " + 
+				"(user_id, quiz_id, final_grade, start_time, time_taken) " + 
+				"VALUES" + 
+				"((SELECT id FROM users WHERE username LIKE ?), ?, ?, ?, ?);";
 	}
 
 	@Override
