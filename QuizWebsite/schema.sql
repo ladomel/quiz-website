ad1125182e1be6a93a379a53880ad482ccefd6d0
Sergi Chalauri <schal13@freeuni.edu.ge>
2016-06-28 21:54:05 +0400

    Tables added to support Result storing
    List of list..

--- QuizWebsite/schema.sql
+++ QuizWebsite/schema.sql
@@ -3,130 +3,135 @@ CREATE SCHEMA oop;
 USE oop;
 
 create table loggedinusers(
-	user_id int(11) not null,
-	primary key(user_id)
+	user_id 			int(11) 		not null,
+	primary 			key(user_id)
 );
 
 create table users(
-	id int(11) not null auto_increment,
-	username varchar(255) not null unique,
-	hash_password varchar(255) not null,
-	salt varchar(255) not null,
-	description longtext,
-	image varchar(255),
+	id 					int(11) 		not null auto_increment,
+	username 			varchar(255) 	not null unique,
+	hash_password 		varchar(255) 	not null,
+	salt 				varchar(255) 	not null,
+	description 		longtext,
+	image 				varchar(255),
 	primary key(id)
 );
 
 insert into users(username, hash_password, salt) values('vaja', '1234', 'salt');
 
 create table friends(
-	first_user_id int(11) not null,
-	second_user_id int(11) not null,
+	first_user_id 		int(11) 		not null,
+	second_user_id 		int(11) 		not null,
 	primary key(first_user_id,second_user_id)
 );
 
 create table status(
-	user_id int(11) not null,
-	status varchar(255) not null,
+	user_id 			int(11) 		not null,
+	status 				varchar(255) 	not null,
 	primary key(user_id,status)
 );
 
 create table achievements(
-	id int(11) not null,
-	name varchar(256) not null,
-	icon varchar(256) not null,
-	description longtext,
+	id 					int(11) 		not null,
+	name 				varchar(256) 	not null,
+	icon 				varchar(256) 	not null,
+	description 		longtext,
 	primary key(id)
 );
 
 create table user_achievements(
-	user_id int(11) not null,
-	achievement_id int(11) not null,
+	user_id 			int(11) 		not null,
+	achievement_id 		int(11) 		not null,
 	primary key(user_id,achievement_id)
 );
 
 create table questions(
-	id int(11) not null auto_increment,
-	quiz_id int(11) not null,
-	problem longtext not null,
-	type varchar(50) not null,
-	grade int(11) not null,
+	id 					int(11) 		not null auto_increment,
+	quiz_id 			int(11) 		not null,
+	problem 			longtext 		not null,
+	type 				varchar(50) 	not null,
+	grade 				int(11) 		not null,
 	primary key(id)
 );
 
 insert into questions (quiz_id, problem, type, grade) values(1, 'whatsup', 'QR', 5);
 
 create table answers(
-	question_id int(11) not null,
-	answer varchar(255) not null,
-	field_id int(11) default 0
+	question_id 		int(11) 		not null,
+	answer 				varchar(255) 	not null,
+	field_id 			int(11) 		default 0
 );
 
 insert into answers (question_id, answer) values(1, 'good'), (1, 'bad');
 
 create table quizzes(
-	id int(11) not null auto_increment,
-	creator_id int(11) not null,
-	name varchar(250) not null,
-	description longtext,
-	is_random boolean not null default 0,
-	is_one_page boolean not null default 1,
-	immediate_correction boolean not null default 0,
-	practice_mode boolean not null default 0, 
-	creation_time bigint not null,
-	time bigint not null,
-	max_score int(11) not null,
-	category varchar(250),
+	id int(11) 			not null 		auto_increment,
+	creator_id 			int(11) 		not null,
+	name 				varchar(250) 	not null,
+	description 		longtext,
+	is_random 			boolean 		not null default 0,
+	is_one_page 		boolean 		not null default 1,
+	immediate_correction boolean 		not null default 0,
+	practice_mode 		boolean 		not null default 0, 
+	creation_time 		bigint 			not null,
+	time 				bigint 			not null,
+	max_score 			int(11) 		not null,
+	category 			varchar(250),
 	primary key(id)
 );
 
 insert into quizzes (creator_id, name, creation_time, time, max_score) values(1, 'birds', 1945, 30, 100);
 
 create table quiz_problems(
-	quiz_id int(11) not null,
-	`index` int(11) not null,
-	question_id int(11) not null,
+	quiz_id 			int(11) 		not null,
+	`index` 			int(11) 		not null,
+	question_id 		int(11) 		not null,
 	primary key(quiz_id, `index`)
 );
 
 create table friendrequests(
-	sender_id int(11) not null,
-	receiver_id int(11) not null,
-	status varchar(50), /* Active, Declined, Accepted */
+	sender_id 			int(11) 		not null,
+	receiver_id 		int(11) 		not null,
+	status 				varchar(50), /* Active, Declined, Accepted */
 	time bigint
 );
 
 create table challenges(
-	sender_id int(11) not null,
-	receiver_id int(11) not null,
-	quiz int(11) not null,
-	status varchar(50), /* Active, Declined, Accepted */
-	time bigint
+	sender_id 			int(11) 		not null,
+	receiver_id 		int(11) 		not null,
+	quiz 				int(11) 		not null,
+	status 				varchar(50), /* Active, Declined, Accepted */
+	time 				bigint
 );
 
 create table notes(
-	sender_id int(11) not null,
-	receiver_id int(11) not null,
-	note longtext,
-	status varchar(50), /* Seen, New */
-	time bigint
+	sender_id 			int(11) 		not null,
+	receiver_id 		int(11) 		not null,
+	note 				longtext,
+	status 				varchar(50), /* Seen, New */
+	time 				bigint
 );
 
-# result related below
+# result related tables below
 
 create table results(
-	id int(11) not null auto_increment,
-	user_id int(11) not null,
-	quiz_id	int(11) not null,
-	final_grade int(11) not null,
-	start_time bigint not null,
-	time_taken bigint not null,
+	id 					int(11) 		not null auto_increment,
+	user_id 			int(11) 		not null,
+	quiz_id				int(11) 		not null,
+	final_grade 		int(11) 		not null,
+	start_time 			bigint 			not null,
+	time_taken 			bigint 			not null,
 	primary key(id)
 );
 
+create table answer_of_question(
+	result_id 			int(11) 		not null,
+	question_id			int(11)  		not null,
+	grade 				int(11)
+);
+
 create table  user_answers(
-	result_id int(11) not null,
-	question_id int(11) not null,
-	user_answer longtext,
-	primary key(result_id, question_id)
+	question_id 		int(11) 		not null,
+	field_id 			int(11) 		not null,
+	user_answer 		longtext
 );
