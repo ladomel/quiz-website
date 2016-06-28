82ca63de964e08efb463c50f555258d1994a8214
Andrew Niedens <niedens.a@gmail.com>
2016-06-28 11:47:57 +0400

    CreateFB implemented

--- QuizWebsite/src/Servlets/CreateFB.java
+++ QuizWebsite/src/Servlets/CreateFB.java
@@ -11,10 +11,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import classes.question.QuestionFB;
-import classes.question.QuestionPR;
 import classes.question.Abstract.Question;
 
 /**
@@ -43,29 +41,23 @@ public class CreateFB extends HttpServlet {
 		String problem = request.getParameter("statement");
 		List<Set<String>> answers = new ArrayList<Set<String>>();
 
-		System.out.println("problem: " + problem);
 		String delimeter = "/";
 		String nextAnswer;
 		for(int i = 0; request.getParameter("answer" + i + delimeter + 0) != null; i++)
 		{
 			nextAnswer = "";
 			Set<String> possibleAnswersSet= new HashSet<String>();
-			for(int j = 0; ; j++)
+			for(int j = 0; ;j++)
 			{
 				nextAnswer = request.getParameter("answer" + i + delimeter + j);
 				if(nextAnswer == null) break;
-			//	System.out.println("answer " + i + delimeter + j + " = " + nextAnswer);
 				possibleAnswersSet.add(nextAnswer);
 			}
 			answers.add(possibleAnswersSet);
 		}
 		
 		QuestionFB questionFB = new QuestionFB(problem, 1, answers);
-		
-		/*
-		HttpSession session = request.getSession();
-		ArrayList<Question> questions = (ArrayList<Question>)session.getAttribute("QuestionList");
-		questions.add(questionMC);
-		*/
+		ArrayList<Question> createdQuestions = (ArrayList<Question>)request.getSession().getAttribute("createdQuestions");
+		createdQuestions.add(questionFB);
 	}
 }
