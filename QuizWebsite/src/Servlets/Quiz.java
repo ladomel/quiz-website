0589daba84dc79e5cef1d184e7f2bcac73b3c096
Andrew Niedens <niedens.a@gmail.com>
2016-06-28 13:09:09 +0400

    Quiz servlet implemented

--- QuizWebsite/src/Servlets/Quiz.java
+++ QuizWebsite/src/Servlets/Quiz.java
@@ -1,11 +1,15 @@
 package Servlets;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.QuizDAO;
 
 /**
  * Servlet implementation class Quiz
@@ -19,23 +23,28 @@ public class Quiz extends HttpServlet {
      */
     public Quiz() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * 
+	 * 
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		int quizId = Integer.parseInt(request.getParameter("id"));
+		QuizDAO quizDAO = (QuizDAO)request.getServletContext().getAttribute("quizDAO");
+		classes.Quiz quiz = quizDAO.getQuiz(quizId);
+		// ArrayList<Questions> questions = quizDAO.getQuestions(quizId);
+		// 	request.setAttribute("Questions", questions);
+		System.out.println("Taken out quiz: " + quiz.toString());
+		request.setAttribute("Quiz", quiz);
+		
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("Quiz.jsp");
+		requestDispatcher.forward(request, response);	
 	}
-
 }
