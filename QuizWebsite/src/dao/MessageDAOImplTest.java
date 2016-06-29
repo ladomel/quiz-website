d17c59ebb8527eb13f86cc4d43dfd494eca74e8c
Andrew Niedens <niedens.a@gmail.com>
2016-06-29 18:08:32 +0400

    Added Announcements

--- QuizWebsite/src/dao/MessageDAOImplTest.java
+++ QuizWebsite/src/dao/MessageDAOImplTest.java
@@ -3,6 +3,7 @@ package dao;
 import org.junit.Before;
 import org.junit.Test;
 
+import classes.Message.Announcement;
 import classes.Message.Challenge;
 import classes.Message.FriendRequest;
 import classes.Message.Note;
@@ -26,8 +27,9 @@ public class MessageDAOImplTest {
 	public void testFriendRequest() {
 		FriendRequest request = classFactory.getFriendRequest("a", 20, "b", false);
 		messageDAO.addFriendRequest(request);
-		
-		//System.out.println(messageDAO.getFriendRequests("b").toString());
+
+		System.out.println(messageDAO.getFriendRequests("b").size());
+		System.out.println(messageDAO.getFriendRequests("b").toString());
 	}
 	
 	@Test
@@ -45,11 +47,15 @@ public class MessageDAOImplTest {
 		Challenge challenge = classFactory.getChallenge("a", 11, "haa", 1, false);
 		messageDAO.addChallenges(challenge);
 		
-	//	System.out.println(messageDAO.getChallenges("haa").size());
-	//	System.out.println(messageDAO.getChallenges("haa").toString());
+		System.out.println(messageDAO.getChallenges("haa").size());
+		System.out.println(messageDAO.getChallenges("haa").toString());
 	}
 	
-	
-	
-	
+	@Test
+	public void testAnnouncement() {
+		Announcement announcement = classFactory.getAnnouncement("a", "announ", 30);
+		messageDAO.addAnnouncement(announcement);
+		
+		System.out.println(messageDAO.getAnnouncements().toString());
+	}
 }
