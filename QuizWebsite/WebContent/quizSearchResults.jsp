3968e7a26dbbed29f18f2a44e7729372cab37cf3
LADO MELIKIDZE <lmeli14@freeuni.edu.ge>
2016-07-05 12:01:45 +0400

    Minor updates and changes: site should be functional

--- QuizWebsite/WebContent/quizSearchResults.jsp
+++ QuizWebsite/WebContent/quizSearchResults.jsp
@@ -1,5 +1,9 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+    
+<%@ page import="classes.*" %>
+
+<%@ page import="java.util.*" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -9,7 +13,12 @@
 <body>
 	<div id="centerpanel">
 		<ul id="list">
-			
+			<%
+				List<Quiz> quizzes = (List<Quiz>)request.getAttribute("Quizzes");
+				for (Quiz quiz: quizzes){
+					out.print("<li> <a href='Quiz?id=" + quiz.getId() + "'> " + quiz.getQuizName() + "</a> </li>");
+				}
+			%>
 		</ul>
 	</div>
 </body>
--- QuizWebsite/src/Servlets/Profile.java
+++ QuizWebsite/src/Servlets/Profile.java
@@ -3,6 +3,7 @@ package Servlets;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
@@ -16,6 +17,8 @@ import javax.servlet.http.HttpSession;
 import Main.Constants;
 import classes.Result;
 import classes.User;
+import dao.AchievementDAO;
+import dao.MessageDAO;
 import dao.QuizDAO;
 import dao.ResultDAO;
 import dao.UserDAO;
@@ -39,33 +42,55 @@ public class Profile extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String userName = request.getParameter("username");
-		System.out.println(userName);
-		RequestDispatcher dispatcher;
+		RequestDispatcher dispatcher = request.getRequestDispatcher("notfound.jsp");
 		ServletContext ctx= getServletContext();  
 		UserDAO userDAO = (UserDAO)ctx.getAttribute("userDAO");
-		dispatcher = request.getRequestDispatcher("notfound.jsp");	
-		
-		QuizDAO quizDao= (QuizDAO)ctx.getAttribute("quizDAO");
-		ResultDAO resultDao= (ResultDAO)ctx.getAttribute("resultDAO");
-		
-		request.setAttribute("createdQuizzes", quizDao.getCreatedQuizzes(userName));
-		
-		List<Result> recent = resultDao.getRecentResults(userName, Constants.MAX_DISPLAY);
-		request.setAttribute("recentResults", recent);
-		List<classes.Quiz> recQuiz= new ArrayList<classes.Quiz>();
-		if (recent!= null){
-		for (Result res : recent){
-			recQuiz.add(quizDao.getQuiz(res.getQuizId()));
-		}}
-		request.setAttribute("recentQuizzes", recQuiz);
+		User master = (User) request.getSession().getAttribute("MasterUser");
 		
 		User user = userDAO.getUser(userName);
-		if(user != null)
-		{
+		if(user != null){
+			QuizDAO quizDao= (QuizDAO)ctx.getAttribute("quizDAO");
+			ResultDAO resultDao= (ResultDAO)ctx.getAttribute("resultDAO");
+			MessageDAO messageDao= (MessageDAO)ctx.getAttribute("messageDAO");
+			
+			Set<String> friends = userDAO.getFriends(userName);
+			
+			request.setAttribute("Friends", friends);
+			request.setAttribute("createdQuizzes", quizDao.getCreatedQuizzes(userName));
+			
 			request.setAttribute("User", user);
-			dispatcher = request.getRequestDispatcher("Profile.jsp?username=" + userName);
+			if (master != null){
+				request.setAttribute("FriendRequestSent", messageDao.friendRequestExists(master.getUserName(), userName));
+				request.setAttribute("FriendRequestReceived", messageDao.friendRequestExists(userName, master.getUserName()));
+			}
+			
+			
+			List<Result> recent = resultDao.getRecentResults(userName, Constants.MAX_DISPLAY);
+			// add sort by time date and score
+			
+			List<classes.Quiz> recQuiz= new ArrayList<classes.Quiz>();
+			if (recent!= null){
+				for (Result res : recent){
+					recQuiz.add(quizDao.getQuiz(res.getQuizId()));
+				}
+			}
+			request.setAttribute("recentResults", recent);
+			request.setAttribute("recentQuizzes", recQuiz);
+			
+			
+			request.setAttribute("FriendsRecent", resultDao.getRecentResults(friends, Constants.MAX_DISPLAY));
+			request.setAttribute("FriendsCreated", null);
+			request.setAttribute("FriendsAchievements", null);
+			
+			
+			//achievements
+			AchievementDAO aD = (AchievementDAO) ctx.getAttribute("achievementDAO");
+			
+			
+			dispatcher = request.getRequestDispatcher("Profile.jsp");
 		}
-		dispatcher.forward(request, response);	
+		
+		dispatcher.forward(request, response);
 	}
 
 	/**
