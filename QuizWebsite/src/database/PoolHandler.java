dde5fafd669843fde418b4fa6bdd970f889097d3
Sergi Chalauri <schal13@freeuni.edu.ge>
2016-07-06 12:01:08 +0400

    Somewhat final version of pooling

--- QuizWebsite/src/database/PoolHandler.java
+++ QuizWebsite/src/database/PoolHandler.java
@@ -4,6 +4,13 @@ import java.util.TimerTask;
 
 import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
 
+/**
+ * Pool adjustment on the fly.
+ * Numbers here are dummy, they just demonstrate pool adjustments.
+ *		 
+ * @author sergi
+ *
+ */
 public class PoolHandler extends TimerTask {
 
 	private BasicDataSource dataSource;
@@ -11,9 +18,10 @@ public class PoolHandler extends TimerTask {
 	private int maxTotal;
 	
 	public PoolHandler(BasicDataSource dataSource2, int nLoggedIn) {
+		
 		this.dataSource = dataSource2;
 		this.minIdle = nLoggedIn / 10 + 1;
-		this.maxTotal = nLoggedIn + 1;
+		this.maxTotal = nLoggedIn / 2 + 1;
 	}
 	
 	@Override
@@ -21,6 +29,7 @@ public class PoolHandler extends TimerTask {
 		dataSource.setMinIdle(minIdle);
 		dataSource.setMaxTotal(maxTotal);
 		
+		// log
 		System.out.println(
 				"Pool size adjusted. # of idle: " + dataSource.getNumIdle() + 
 				", max total: " + dataSource.getMaxTotal());
